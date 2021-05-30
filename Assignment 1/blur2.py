94bc4f259dc4ce90703b21f01becbb351002fad1
ojaswijain <81473548+ojaswijain@users.noreply.github.com>
2021-05-31 01:41:52 +0530

    Update blur2.py

--- Assignment 1/blur2.py
+++ Assignment 1/blur2.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 MAX_KERNEL_LENGTH = 31
 dst = None
-window_name = 'Smoothing Demo'
+window_name = 'Gaussian'
 src=cv.imread(argv[0])
 for i in range(1, MAX_KERNEL_LENGTH, 2):
         dst = cv.GaussianBlur(src, (i, i), 5)
