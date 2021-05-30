99c1284463c04b143be6a567a7e0e32677e27663
ojaswijain <81473548+ojaswijain@users.noreply.github.com>
2021-05-31 01:38:29 +0530

    Update blur2.py

--- Assignment 1/blur2.py
+++ Assignment 1/blur2.py
@@ -2,10 +2,10 @@ import cv2 as cv
 MAX_KERNEL_LENGTH = 31
 dst = None
 window_name = 'Smoothing Demo'
-src=cv.imread('vr.png')
+src=cv.imread(argv[0])
 for i in range(1, MAX_KERNEL_LENGTH, 2):
         dst = cv.GaussianBlur(src, (i, i), 5)
 cv.imshow(window_name,dst)
 k = cv.waitKey(0)
 if k == ord("s"):
-    cv.imwrite("vaibhav.jpg", dst)
+    cv.imwrite("v.jpg", dst)
